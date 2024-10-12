40efaabc6bc680cd346b1ba60e4d2674db7a7a6c
akinsteph <stephthedeveloper@gmail.com>
2024-10-12 12:12:42 +0100

    remove comments and unused lines

--- wp-content/themes/coolkidsnetwork/templates/avatar-template.php
+++ wp-content/themes/coolkidsnetwork/templates/avatar-template.php
@@ -10,22 +10,12 @@ if (!is_user_logged_in()) {
 
 get_header();
 
-// $character_management = CoolKidsNetwork\Features\CharacterManagement::get_instance();
-// $user_data = $character_management->get_user_data(get_current_user_id());
-// var_dump($user_data);
 ?>
 
 <main class="avatar-profile">
 	<div class="container">
 		<h1><?php echo esc_html__('Your Character', 'cool-kids-network'); ?></h1>
-
-
 		<?php echo do_shortcode('[cool_kids_character_data]'); ?>
-		<!-- <div class="character-details">
-			<?php foreach ($user_data as $key => $value) : ?>
-				<p><strong><?php echo esc_html(ucfirst(str_replace('_', ' ', $key))); ?>:</strong> <?php echo esc_html($value); ?></p>
-			<?php endforeach; ?>
-		</div> -->
 	</div>
 </main>
 
